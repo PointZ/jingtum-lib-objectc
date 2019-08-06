483316c420793e043772a188e56628ab7264c9bb
PointZ <zhoudiancheng0803@qq.com>
2019-08-06 10:03:57 +0800

    keyStoreFile update on kdfParams

--- WebSocketClient/jingtum-lib/KeyStoreFile.h
+++ WebSocketClient/jingtum-lib/KeyStoreFile.h
@@ -11,7 +11,7 @@
 
 #import <Foundation/Foundation.h>
 
-
+//227-262
 @interface CipherParams : NSObject
 {
     NSString *iv;
@@ -21,6 +21,59 @@
 -(BOOL)isEqual:(id)object;
 -(NSUInteger)hash;
 @end
+
+//264-268
+@protocol KdfParams <NSObject>
+@required
+-(NSUInteger)getDklen;
+-(NSString *)getSalt;
+@end
+
+//270-346
+@interface Aes128CtrKdfParams: NSObject<KdfParams>
+{
+    int dklen;
+    int c;
+    NSString *prf;
+    NSString *salt;
+}
+-(int)getDklen;
+-(void)setDklen:(int)_dklen;
+-(int)getC;
+-(void)setC:(int)_c;
+-(NSString *)getPrf;
+-(void)setPrf:(NSString *)_prf;
+-(NSString *)getSalt;
+-(void)setSalt:(NSString *)_salt;
+-(BOOL)isEqual:(id)object;
+-(NSUInteger)hash;
+
+@end
+
+//348-435
+@interface ScryptKdfParams: NSObject<KdfParams>
+{
+    int dklen;
+    int n;
+    int p;
+    int r;
+    NSString *salt;
+}
+-(int)getDklen;
+-(void)setDklen:(int)_dklen;
+-(int)getN;
+-(void)setN:(int)_n;
+-(int)getP;
+-(void)setP:(int)_p;
+-(int)getR;
+-(void)setR:(int)_r;
+-(NSString *)getSalt;
+-(void)setSalt:(NSString *)_salt;
+-(BOOL)isEqual:(id)object;
+-(NSUInteger)hash;
+
+@end
+
 
 @interface Crypto : NSObject
 {
--- WebSocketClient/jingtum-lib/KeyStoreFile.m
+++ WebSocketClient/jingtum-lib/KeyStoreFile.m
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "KeyStoreFile.h"
 
+//227-262
 @implementation CipherParams
 
 -(NSString *)getIv
@@ -44,6 +45,184 @@
 }
 
 @end
+
+/**********************************************/
+
+//270-346
+@implementation Aes128CtrKdfParams
+
+-(id)init
+{
+    self = [super init];
+    return self;
+}
+-(int)getDklen
+{
+    return dklen;
+}
+-(void)setDklen:(int)_dklen
+{
+    dklen = _dklen;
+}
+-(int)getC
+{
+    return c;
+}
+-(void)setC:(int)_c
+{
+    c = _c;
+}
+-(NSString *)getPrf
+{
+    return prf;
+}
+-(void)setPrf:(NSString *)_prf
+{
+    prf = _prf;
+}
+-(NSString *)getSalt
+{
+    return salt;
+}
+-(void)setSalt:(NSString *)_salt
+{
+    salt = _salt;
+}
+-(BOOL)isEqual:(id)object
+{
+    if (self == object)
+    {
+        return YES;
+    }
+    
+    if (![object isKindOfClass:[Aes128CtrKdfParams class]])
+    {
+        return NO;
+    }
+    
+    Aes128CtrKdfParams* obj = (Aes128CtrKdfParams*)object;
+    if(dklen != [obj getDklen])
+    {
+        return NO;
+    }
+    if(c != [obj getC])
+    {
+        return NO;
+    }
+    if(prf != nil ? ![prf isEqualToString:[obj getPrf]] : [obj getPrf] != nil)
+    {
+        return NO;
+    }
+    return salt != nil ? [salt isEqualToString:[obj getSalt]] : [obj getSalt] == nil;
+}
+-(NSUInteger)hash
+{
+    NSUInteger result = dklen;
+    result = 31*result+c;
+    result = 31*result+(prf != nil ? [prf hash]:0);
+    result = 31*result+(salt != nil ? [salt hash]:0);
+    return result;
+}
+
+@end
+
+/**********************************************/
+
+//348-435
+@implementation ScryptKdfParams
+
+-(id)init
+{
+    self = [super init];
+    return self;
+}
+-(int)getDklen
+{
+    return dklen;
+}
+-(void)setDklen:(int)_dklen
+{
+    dklen = _dklen;
+}
+-(int)getN
+{
+    return n;
+}
+-(void)setN:(int)_n
+{
+    n = _n;
+}
+-(int)getP
+{
+    return p;
+}
+-(void)setP:(int)_p
+{
+    p = _p;
+}
+-(int)getR
+{
+    return r;
+}
+-(void)setR:(int)_r
+{
+    r = _r;
+}
+
+-(NSString *)getSalt
+{
+    return salt;
+}
+-(void)setSalt:(NSString *)_salt
+{
+    salt = _salt;
+}
+
+-(BOOL)isEqual:(id)object
+{
+    if (self == object)
+    {
+        return YES;
+    }
+    
+    if (![object isKindOfClass:[ScryptKdfParams class]])
+    {
+        return NO;
+    }
+    
+    ScryptKdfParams* obj = (ScryptKdfParams*)object;
+    if(dklen != [obj getDklen])
+    {
+        return NO;
+    }
+    if(n != [obj getN])
+    {
+        return NO;
+    }
+    if(p != [obj getP])
+    {
+        return NO;
+    }
+    if(r != [obj getR])
+    {
+        return NO;
+    }
+
+    return salt != nil ? [salt isEqualToString:[obj getSalt]] : [obj getSalt] == nil;
+}
+-(NSUInteger)hash
+{
+    NSUInteger result = dklen;
+    result = 31*result+n;
+    result = 31*result+p;
+    result = 31*result+r;
+    result = 31*result+(salt != nil ? [salt hash]:0);
+    return result;
+}
+
+@end
+
+
 
 @implementation Crypto
 
