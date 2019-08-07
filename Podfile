7982c7dcea800018cb6d8fbd6eec376e14c4e7f7
PointZ <zhoudiancheng0803@qq.com>
2019-08-07 17:10:34 +0800

    SCrypt 算法解决

--- Podfile
+++ Podfile
@@ -1,4 +1,5 @@
 target 'WebSocketClient' do
 	pod 'SocketRocket'
+	pod "NAChloride"
 	pod 'CoreBitcoin', :podspec => 'https://raw.github.com/oleganza/CoreBitcoin/master/CoreBitcoin.podspec'
 end
--- WebSocketClient/main.m
+++ WebSocketClient/main.m
@@ -9,8 +9,25 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+#import "NAChloride.h"
+#import "Wallet.h"
+#import "Seed.h"
 int main(int argc, char * argv[]) {
+    NAChlorideInit();
+    NSString *secret = @"shExMjiMqza4DdMaSg3ra9vxWPZsQ";
+    Seed * seed = [Seed alloc];
+    Keypairs *keypairs = [seed deriveKeyPair:secret];
+    Wallet *wallet = [[Wallet alloc]initWithKeypairs:keypairs private:secret];
+    
+    
+    
     NSLog(@"git test");
+    NSData *key = [@"Key123456" dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *salt = [NARandom randomData:32];
+    NSError *error = nil;
+    NSData *derivedKey = [NAScrypt scrypt:key salt:salt N:1<<12 r:8 p:6 length:32 error:&error];
+    
+    NSLog(@"%@",derivedKey);
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
