48e265eed4f6ef9d415b7418fd9ef34cf048e175
PointZ <zhoudiancheng0803@qq.com>
2019-08-09 15:51:32 +0800

    Keccak 算法提交

--- WebSocketClient/main.m
+++ WebSocketClient/main.m
@@ -12,13 +12,19 @@
 #import "NAChloride.h"
 #import "Wallet.h"
 #import "Seed.h"
+
+#import "NADigest.h"
+#import "NAKeccak.h"
+//#import "NASHA3.h"
+
 int main(int argc, char * argv[]) {
     NAChlorideInit();
     NSString *secret = @"shExMjiMqza4DdMaSg3ra9vxWPZsQ";
     Seed * seed = [Seed alloc];
     Keypairs *keypairs = [seed deriveKeyPair:secret];
     Wallet *wallet = [[Wallet alloc]initWithKeypairs:keypairs private:secret];
-    
+    NSData *data =[@"Key123456" dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *sha = [NAKeccak SHA3ForData:data digestBitLength:512];
     
     
     NSLog(@"git test");
@@ -28,6 +34,8 @@ int main(int argc, char * argv[]) {
     NSData *derivedKey = [NAScrypt scrypt:key salt:salt N:1<<12 r:8 p:6 length:32 error:&error];
     
     NSLog(@"%@",derivedKey);
+    NSLog(@"%@",sha);
+    //NSLog(@"%@",derivedKey);
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
