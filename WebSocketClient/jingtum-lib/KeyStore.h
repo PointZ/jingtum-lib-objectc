c40dfb5e60befbda2a32f40b3fbf1e03c18574e3
PointZ <zhoudiancheng0803@qq.com>
2019-08-12 17:12:49 +0800

    KeyStore 开始

--- WebSocketClient/jingtum-lib/KeyStore.h
+++ WebSocketClient/jingtum-lib/KeyStore.h
@@ -9,4 +9,15 @@
 #ifndef KeyStore_h
 #define KeyStore_h
 
+#import "KeyStoreFile.h"
+#import "Wallet.h"
+
+@interface KeyStore : NSObject
+
++(KeyStoreFileModel*)createStandard:(NSString*)password wallet:(Wallet*)wallet;
++(KeyStoreFileModel*)createLight:(NSString*)password wallet:(Wallet*)wallet;
++(KeyStoreFileModel*)create:(NSString*)password wallet:(Wallet*)wallet n:(int)n p:(int)p;
+
+@end
+
 #endif /* KeyStore_h */
